6f57fd748b154ab64e33e394af5e619ae4af51af
Kirill Mokhov <vxruuu@gmaIl.com>
2026-03-02 00:39:26 +0300

    sss

--- lib/minio.ts
+++ lib/minio.ts
@@ -2,13 +2,13 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
 export const s3Client = new S3Client({
-  endpoint: `https://${process.env.MINIO_ENDPOINT || 'clipreel/s3'}:${process.env.MINIO_PORT || '9000'}`,
+  endpoint: `http://${process.env.MINIO_ENDPOINT || 'localhost'}:${process.env.MINIO_PORT || '9000'}`,
   region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.MINIO_ACCESS_KEY || 'minioadmin',
     secretAccessKey: process.env.MINIO_SECRET_KEY || 'AQVNwMXBHzn5LmJC_tVpH0T8xIhYDQQbyYCiH5Mg',
   },
-  forcePathStyle: true, // Обязательно для MinIO!
+  forcePathStyle: true,
 });
 
 export const BUCKET_NAME = process.env.MINIO_BUCKET || 'media';
