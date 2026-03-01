dec8e46b19ba0877559b56f53f5a5cc329aad823
Kirill Mokhov <vxruuu@gmaIl.com>
2026-03-01 23:30:31 +0300

    3232

--- app/(admin)/file/page.tsx
+++ app/(admin)/file/page.tsx
@@ -2,19 +2,22 @@
 import { RedditStory } from "@/components/forms/reddit-story-form"
 import { VoiceStory } from "@/components/forms/voice-form"
 import { auth } from "@/lib/auth"
-import { NextResponse } from "next/server";
-import { headers } from "next/headers";
+import { redirect } from "next/navigation"
+import { headers } from "next/headers"
 
 export default async function Home() {
   const session = await auth.api.getSession({
     headers: await headers()
   })
+
   if (!session || session.user.role !== 'admin') {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    redirect('/singin') // или '/' куда нужно
   }
-  return ( <>
-    <RedditStory /> 
-    <VoiceStory />
+
+  return (
+    <>
+      <RedditStory /> 
+      <VoiceStory />
     </>
   )
 }
--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,17 +1,16 @@
-
-import {headers} from 'next/headers'
-import {auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { auth } from '@/lib/auth'
 import { redirect } from "next/navigation"
+
 export default async function Layout({ children }: { children: React.ReactNode }) {
-    const session = await auth.api.getSession({ headers: await headers() })
-    
-    if (session == null) return redirect("/")
-    const hasAccess = await auth.api.userHasPermission({
-      headers: await headers(),
-      body: { permission: { user: ["list"] } },
-    })
-    if (!hasAccess.success) return redirect("/")
-    
-        return ( 
-            {children}
-        )}
+  const session = await auth.api.getSession({ headers: await headers() })
+  if (session == null) return redirect("/")
+  
+  const hasAccess = await auth.api.userHasPermission({
+    headers: await headers(),
+    body: { permission: { user: ["list"] } },
+  })
+  if (!hasAccess.success) return redirect("/")
+  
+  return <>{children}</>
+}
