b6c3e3be2c8ae7865bb392467308761dd094cfa2
Kirill Mokhov <vxruuu@gmaIl.com>
2026-03-01 21:24:22 +0300

    322332

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { adminClient, phoneNumberClient, twoFactorClient, organizationClient } from "better-auth/client/plugins"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost", 
+    baseURL: "https://clipreel.ru", 
      plugins: [
         adminClient(),
         phoneNumberClient(),
